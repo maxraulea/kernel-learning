77eb3b934a9750328897c2c850e5126b072433ed
maxraulea <max.raulea@gmail.com>
2024-11-03 19:27:03 +0100

    write cursor position

--- x86-OS-src/loader.s
+++ x86-OS-src/loader.s
@@ -15,12 +15,21 @@ align 4					;the code must be 4 bytes alligned ?
 loader:					;this is the entry point for the executable
 	mov eax, 0xCAFEBABE		;place number in register eax
 	mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the                                                ; stack (end of memory area)
-
-    	
+	
+	
+	mov word [0x000B8000], 0x2841    	
 	push dword 3            ; arg3
     	push dword 2            ; arg2
     	push dword 1            ; arg1
     	call sum_of_three       ; call the function, the result will be in eax
+	mov al, 0x0D
+	out  0x3D4, al      ; 14 tells the framebuffer to expect the highest 8 bits of the position
+	mov al, 0x0F
+	out 0x3D5, al    ; sending the highest 8 bits of 0x0050
+	mov al, 15
+	out 0x3D4, al      ; 15 tells the framebuffer to expect the lowest 8 bits of the position
+	mov al, 0xFF
+	out 0x3D5, al    ; sending the lowest 8 bits of 0x0050
 
 .loop:
 	jmp .loop			;infinite loop
