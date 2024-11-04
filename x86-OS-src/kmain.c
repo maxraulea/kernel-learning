0dedf774953986c0f516431986213c58f45cbabe
maxraulea <max.raulea@gmail.com>
2024-11-04 19:16:31 +0100

    added scroll functionality when writing to the console TODO: make memset function

--- x86-OS-src/kmain.c
+++ x86-OS-src/kmain.c
@@ -8,8 +8,11 @@ int sum_of_three(int arg1, int arg2, int arg3)
 void kmain(){
 	char message[] = "ik ben max";
 	char message2[] = "\nlet's test what happens";
-	//while(1){
+	char message3[] = "\n jo boys";
+	for(unsigned int i = 0; i < 12; i++){
 		write_to_framebuffer(message, 10);
 		write_to_framebuffer(message2, 24);
-	//}
+		write_to_framebuffer(message3, 9);
+	}
+	write_to_framebuffer(message3, 9);
 }
--- x86-OS-src/write_framebuffer.c
+++ x86-OS-src/write_framebuffer.c
@@ -6,6 +6,11 @@
 /* The I/O port commands */
 #define FB_HIGH_BYTE_COMMAND    14
 #define FB_LOW_BYTE_COMMAND     15
+
+#define FB_CHAR_WIDTH 80
+#define FB_CHAR_HEIGHT 25
+
+#define FB_ADDR 0x000B8000
 /** fb_write_cell:
 
 *  Writes a character with the given foreground and background to position i
@@ -18,7 +23,7 @@
 */
 void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
 {
-	char *fb = (char *) 0x000B8000;
+	char *fb = (char *) FB_ADDR;
 	fb[i] = c;
 	fb[i + 1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
 }
@@ -36,14 +41,51 @@ void fb_move_cursor(unsigned short pos)
 	outb(FB_DATA_PORT,    pos & 0x00FF);
 }
 
+/* TODO can be optimized for using int* instead if char* */
+void scroll()
+{
+
+	int* fb = (int*) FB_ADDR;
+	
+	for(unsigned int i = 0; i < FB_CHAR_HEIGHT; i++)
+	{	
+		for(unsigned int j = 0; j < FB_CHAR_WIDTH / 2; j++)
+		{		
+			int fb_old = j + i * FB_CHAR_WIDTH / 2;
+			int fb_new = j + (i + 1) * FB_CHAR_WIDTH / 2;
+			fb[fb_old] = fb[fb_new];
+		}	
+	}
+	
+	for(unsigned int i = 0; i < FB_CHAR_WIDTH; i++){
+		int last_row_index = FB_CHAR_WIDTH * (FB_CHAR_HEIGHT) + i;
+		fb[last_row_index] = 0x00;
+	}
+	// set the lowest row to 0
+}
+
 void write_to_framebuffer(char* buf, unsigned int len)
 {	
 	static unsigned int framebuffer_index = 0;
 	
 	for(unsigned int i = 0; i < len; i++)
 	{
-		fb_write_cell(framebuffer_index * 2, buf [i], 2, 0);
-		fb_move_cursor(framebuffer_index);
+		if(buf[i] == '\n')
+		{
+			unsigned int char_till_end_line = 80 - (framebuffer_index % FB_CHAR_WIDTH);
+			framebuffer_index += char_till_end_line;
+			continue;
+		}
+		// we check if the frame buffer will enter the region out of the screen
+		// then we have to shift the memory one row up and set the lowest line to 0
+		if(framebuffer_index >= FB_CHAR_WIDTH * FB_CHAR_HEIGHT)
+		{
+			scroll();
+			framebuffer_index = FB_CHAR_WIDTH * (FB_CHAR_HEIGHT - 1);	
+		}	
+		
+		fb_write_cell(framebuffer_index * 2, buf[i], 2, 0);
+		fb_move_cursor(framebuffer_index + 1);
 		framebuffer_index++;
 	}
 }
