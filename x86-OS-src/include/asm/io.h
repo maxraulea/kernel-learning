20acced4ea7ccef40405a0b45d9c5fc5975f974a
maxraulea <max.raulea@gmail.com>
2024-11-10 15:46:21 +0100

    added functions given by the book

--- x86-OS-src/include/asm/io.h
+++ x86-OS-src/include/asm/io.h
@@ -4,4 +4,10 @@
 #define outb(value, port) \
 	__asm__ ("outb %%al,%%dx"::"a" ((unsigned char)(value)),"d" ((unsigned short)(port)))
 
+#define inb(port) ({ \
+unsigned char _v; \
+__asm__ volatile ("inb %%dx,%%al":"=a" (_v):"d" (port)); \
+_v; \
+})
+
 #endif
---
+++ x86-OS-src/include/maxos/write_serial.h
@@ -0,0 +1,6 @@
+#ifndef write_serial_h
+#define write_serial_h
+
+void write_buffer_to_serial(const char* buf, unsigned int len);
+
+#endif
